2fc0b215c04d79229cb283fd332f3814f6286fc2
a2c6201 <a2c6201@gmail.com>
2022-11-19 15:29:06 +0900

    環境変数を受けとれるように

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import tweepy
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
 
 # 認証に必要なキーとトークン
-# API_KEY = os.environ.get('API_KEY')
-# API_SECRET = os.environ.get('API_SECRET')
-# ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
-# ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+API_KEY = os.environ.get('API_KEY')
+API_SECRET = os.environ.get('API_SECRET')
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 
 # TwitterAPI認証用関数
 def authTwitter():
