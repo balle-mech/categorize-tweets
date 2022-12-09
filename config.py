495563617264066fcf1fc31cceadfbc507863cd6
a2c6201 <a2c6201@gmail.com>
2022-12-09 23:43:46 +0900

    add: dotenvをコメント解除

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import tweepy
 
-# dotenv_path = join(dirname(__file__), '.env')
-# load_dotenv(dotenv_path)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # 認証に必要なキーとトークン
 API_KEY = os.environ.get('API_KEY')
